313b0033c52088212989c17cd0f6cc3a1fabdd45
InputUsername <InputUsername@users.noreply.github.com>
2015-06-22 12:42:41 +0200

    Update whiteboard.js

--- client/whiteboard.js
+++ client/whiteboard.js
@@ -15,14 +15,14 @@ var showInterface = function(showCanvas) {
         $canvas.style.display = "inline-block";
         $connection.style.display = "none";
     }
-}
+};
 
 showInterface(false);
 
 // Connection stuff
 var makeUrl = function(host, port) {
     return "ws://" + host + ":" + port + "/whiteboard";
-}
+};
 
 socket = null;
 
@@ -45,114 +45,11 @@ ctx.clearRect(0, 0, $canvas.width, $canvas.height);
 ctx.fillStyle = "#ffffff";
 ctx.fillRect(0, 0, $canvas.width, $canvas.height);
 
-/*
-var painting = false;
-var drawQueue = [];
-var dragQueue = [];
-
-var line = function(x1, y1, x2, y2, func) {
-    var deltaX = Math.abs(x2 - x1);
-    var deltaY = Math.abs(y2 - y1);
-
-    if (deltaX == 0) {
-        var startY = Math.min(y1, y2);
-        var endY = Math.max(y1, y2);
-
-        for (var y = startY; y <= endY; y++) {
-            func(x1, y);
-        }
-    }
-
-    var slope = Math.round(deltaY / deltaX);
-    var startX = Math.min(x1, x2);
-    var endX = Math.max(x1, x2);
-
-    for (var x = startX; x <= endX; x++) {
-        var y = Math.min(y1, y2) + (slope * x);
-
-        func(x, y);
-    }
-}
-
-var redraw = function(clear) {
-    if (clear) {
-        ctx.strokeStyle = "#ffffff";
-    }
-    else {
-        ctx.strokeStyle = "#000000";
-    }
-
-    ctx.lineJoin = "round";
-    ctx.lineWidth = 5;
-
-    var length = Math.min(drawQueue.length, dragQueue.length);
-
-    for (var i = 0; i < length; i++) {
-        if (dragQueue[i] && i) {
-            line(drawQueue[i - 1][0], drawQueue[i - 1][1],
-                drawQueue[i][0], drawQueue[i][1],
-                function(x, y) {
-                    ctx.moveTo(drawQueue[i][0] - 1, drawQueue[i][1]);
-                    ctx.lineTo(drawQueue[i][0], drawQueue[i][1]);
-                    ctx.closePath();
-                    ctx.stroke();
-
-                    socket.send("p_" + x + "_" + y);
-                });
-        }
-        else {
-            ctx.moveTo(drawQueue[i][0] - 1, drawQueue[i][1]);
-            ctx.lineTo(drawQueue[i][0], drawQueue[i][1]);
-            ctx.closePath();
-            ctx.stroke();
-
-            socket.send("p_" + drawQueue[i][0] + "_" + drawQueue[i][1]);
-        }
-    }
-
-    drawQueue = [];
-    dragQueue = [];
-}
-
-$canvas.addEventListener("mousedown", function(event) {
-    if (socket == null) {
-        return;
-    }
-
-    var mouseX = event.pageX - this.offsetLeft;
-    var mouseY = event.pageY - this.offsetTop;
-    drawQueue.push( [mouseX, mouseY] );
-    dragQueue.push(false);
-
-    redraw(false);
-
-    painting = true;
-}, false);
-
-$canvas.addEventListener("mousemove", function(event) {
-    if (socket == null) {
-        return;
-    }
-
-    if (painting) {
-        var mouseX = event.pageX - this.offsetLeft;
-        var mouseY = event.pageY - this.offsetTop;
-        drawQueue.push( [mouseX, mouseY] );
-        dragQueue.push(true);
-
-        redraw(false);
-    }
-}, false);
-
-$canvas.addEventListener("mouseup", function(event) {
-    if (socket == null) {
-        return;
-    }
-
-    painting = false;
-}, false);
-
-*/
+var clearCanvas = function() {
+    $canvas.clearRect(0, 0, $canvas.width, $canvas.height);
+    $canvas.fillStyle = "#ffffff";
+    $canvas.fillRect(0, 0, $canvas.width, $canvas.height);
+};
 
 // Connection stuff
 $connect.addEventListener("click", function() {
@@ -173,7 +70,7 @@ $connect.addEventListener("click", function() {
 
         socket.onmessage = function(msg) {
             console.log("got message:");
-            //console.log(msg);
+            console.log(msg);
         }
 
         socket.onerror = function(error) {
@@ -183,36 +80,21 @@ $connect.addEventListener("click", function() {
 
         socket.onclose = function() {
             console.log("connection closed");
-
+            
             reset();
         }
     }
     catch (error) {
         console.log("can't connect to " + url);
-
+        
         reset();
     }
 
 }, false);
 
 // General stuff
-var clearCanvas = function() {
-    $canvas.clearRect(0, 0, $canvas.width, $canvas.height);
-    $canvas.fillStyle = "#ffffff";
-    $canvas.fillRect(0, 0, $canvas.width, $canvas.height);
-}
-
-/*
-
-var begin = function() {
-    console.log("")
-}
-
 var reset = function() {
-    console.log("reset");
-    socket.close();
-    socket = null;
+    clearCanvas();
     showInterface(false);
-}
-
-*/
+    socket = null;
+};
