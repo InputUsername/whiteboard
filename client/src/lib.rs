6e1f69a792151d4529291611a30aae4d183150cf
Koen Bolhuis <koen.bolhuis@gmail.com>
2020-06-04 19:42:37 +0200

    Split WebSocket messages to separate enum

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -15,13 +15,17 @@ struct Model {
     ws: Option<WebSocketTask>,
 }
 
+enum WsUpdate {
+    Message,
+    Opened,
+    Closed,
+    Error,
+}
+
 enum Msg {
     Connect(String),
     Disconnect,
-    WsMsg,
-    WsOpened,
-    WsClosed,
-    WsError,
+    WsUpdate(WsUpdate),
 }
 
 fn alert(s: &str) {
@@ -60,11 +64,11 @@ impl Component for Model {
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::Connect(addr) => {
-                let callback = self.link.callback(|_data: IgnoreData| Msg::WsMsg);
+                let callback = self.link.callback(|_data: IgnoreData| Msg::WsUpdate(WsUpdate::Message));
                 let notification = self.link.callback(|status| match status {
-                    WebSocketStatus::Opened => Msg::WsOpened,
-                    WebSocketStatus::Closed => Msg::WsClosed,
-                    WebSocketStatus::Error => Msg::WsError,
+                    WebSocketStatus::Opened => Msg::WsUpdate(WsUpdate::Opened),
+                    WebSocketStatus::Closed => Msg::WsUpdate(WsUpdate::Closed),
+                    WebSocketStatus::Error => Msg::WsUpdate(WsUpdate::Error),
                 });
                 let task = self
                     .ws_service
@@ -73,15 +77,17 @@ impl Component for Model {
                 self.ws = Some(task);
             }
             Msg::Disconnect => self.ws = None,
-            Msg::WsMsg => alert("Message received"),
-            Msg::WsOpened => alert("Opened"),
-            Msg::WsClosed => {
-                alert("Closed");
-                self.ws = None;
-            }
-            Msg::WsError => {
-                alert("Error");
-                self.ws = None;
+            Msg::WsUpdate(update) => match update {
+                WsUpdate::Message => alert("Message received"),
+                WsUpdate::Opened => alert("Opened"),
+                WsUpdate::Closed => {
+                    alert("Closed");
+                    self.ws = None;
+                }
+                WsUpdate::Error => {
+                    alert("Error");
+                    self.ws = None;
+                }
             }
         }
         true
