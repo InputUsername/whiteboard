372768dd48ad1cda44a122c19e3be47c81b0edd7
Koen Bolhuis <koen.bolhuis@gmail.com>
2020-06-05 14:26:16 +0200

    Implement drawing on canvas

--- client/src/draw.rs
+++ client/src/draw.rs
@@ -1,26 +1,48 @@
 use yew::prelude::*;
-use web_sys::{HtmlCanvasElement, CanvasRenderingContext2d};
-use wasm_bindgen::JsCast;
+use web_sys::{HtmlCanvasElement, CanvasRenderingContext2d, MouseEvent};
+use wasm_bindgen::{JsValue, JsCast};
 
 const NO_CANVAS: &str = "Oops! Looks like your browser does not support the Canvas element.";
 
 pub struct DrawArea {
     link: ComponentLink<Self>,
+    props: Props,
+
     node_ref: NodeRef,
     canvas: Option<HtmlCanvasElement>,
     ctx: Option<CanvasRenderingContext2d>,
+
+    drawing: bool,
+    x: i32,
+    y: i32,
+}
+
+pub enum Msg {
+    MouseDown { x: i32, y: i32 },
+    MouseMove { x: i32, y: i32 },
+    MouseUp { x: i32, y: i32 },
+}
+
+#[derive(Clone, PartialEq, Properties)]
+pub struct Props {
+    pub width: f64,
+    pub height: f64,
 }
 
 impl Component for DrawArea {
-    type Message = ();
-    type Properties = ();
+    type Message = Msg;
+    type Properties = Props;
 
-    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
+            props,
             node_ref: NodeRef::default(),
             canvas: None,
             ctx: None,
+            drawing: false,
+            x: 0,
+            y: 0,
         }
     }
 
@@ -36,24 +58,80 @@ impl Component for DrawArea {
 
         self.canvas = Some(canvas);
         self.ctx = Some(ctx);
+
+        self.clear_canvas();
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::MouseDown { x, y } => {
+                self.drawing = true;
+                self.x = x;
+                self.y = y;
+            }
+            Msg::MouseMove { x, y } => {
+                if self.drawing {
+                    self.draw_line(self.x, self.y, x, y);
+                    self.x = x;
+                    self.y = y;
+                }
+            }
+            Msg::MouseUp { x, y } => {
+                if self.drawing {
+                    self.draw_line(self.x, self.y, x, y);
+                    self.drawing = false;
+                    self.x = 0;
+                    self.y = 0;
+                }
+            }
+        }
         false
     }
 
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if props != self.props {
+            self.props = props;
+            return true;
+        }
         false
     }
 
     fn view(&self) -> Html {
+        let mousedown = self.link.callback(|e: MouseEvent| Msg::MouseDown { x: e.offset_x(), y: e.offset_y() });
+        let mousemove = self.link.callback(|e: MouseEvent| Msg::MouseMove { x: e.offset_x(), y: e.offset_y() });
+        let mouseup = self.link.callback(|e: MouseEvent| Msg::MouseUp { x: e.offset_x(), y: e.offset_y() });
         html! {
             <canvas ref={self.node_ref.clone()}
                 id="draw-canvas"
-                width=1600
-                height=900>
+                width=self.props.width
+                height=self.props.height
+                onmousedown=mousedown
+                onmousemove=mousemove
+                onmouseup=mouseup>
                 { NO_CANVAS }
             </canvas>
+        }
+    }
+}
+
+impl DrawArea {
+    fn clear_canvas(&self) {
+        if let Some(ref ctx) = self.ctx {
+            ctx.clear_rect(0.0, 0.0, self.props.width, self.props.height);
+            ctx.set_fill_style(&JsValue::from_str("white"));
+            ctx.fill_rect(0.0, 0.0, self.props.width, self.props.height);
+        }
+    }
+
+    fn draw_line(&self, x1: i32, y1: i32, x2: i32, y2: i32) {
+        if let Some(ref ctx) = self.ctx {
+            ctx.begin_path();
+            ctx.set_stroke_style(&JsValue::from_str("black"));
+            ctx.set_line_width(2.0);
+            ctx.move_to(f64::from(x1), f64::from(y1));
+            ctx.line_to(f64::from(x2), f64::from(y2));
+            ctx.stroke();
+            ctx.close_path();
         }
     }
 }
